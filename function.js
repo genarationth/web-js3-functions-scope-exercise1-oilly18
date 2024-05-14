b07ccd45df863e2699356eb9a99bd014ab4b4266
oilly18 <oilly18@hotmail.com>
2024-05-14 13:53:55 +0700

    functioncommit

---
+++ function.js
@@ -0,0 +1,37 @@
+//1.1
+function addNumbers(a, b) {
+      return (a + b);
+  }
+
+console.log (addNumbers (20,3));
+
+
+
+function addNumber(a, b) {
+    return (a - b);
+}
+console.log (addNumber(15, 2));
+
+
+
+function addNumber(a, b) {
+    return (a * b);
+}
+console.log (addNumber (15, 5));
+
+
+function addNumber(a, b) {
+    return (a / b);
+}
+console.log (addNumber(50, 5));
+
+//1.2
+ // function greet1(name, age) {
+  //   console.log(`Hello, ${name}! You are ${age} years old.`);
+  // }
+  
+  // greet1("Jill"); 
+function greet(firstName, location, hobby ) {
+console.log("Hi, my name is " + firstName +"." + " I live in " + location + " and enjoy "+hobby + ".");
+}
+greet('Micheal', 'Bangkok', 'play game');
